147dc0d566e2c5551078488ae8e029f03964d546
olumoyin titobioluwa joshua <olumoyinjoshua19609@gmail.com git config --global user.email olumoyinjoshua19609@gmail.com>
2023-02-16 16:48:15 -0800

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#include <stdio.h>
+/**
+ * *main - Entry point function
+ * *Return:This will Always 0 that is(interger) (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
