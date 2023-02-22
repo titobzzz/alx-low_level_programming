93c1b1ab23aa99f0211c2fd8e8fc642ce6e87fa9
olumoyin titobioluwa joshua <olumoyinjoshua19609@gmail.com git config --global user.email olumoyinjoshua19609@gmail.com>
2023-02-21 19:24:27 -0800

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,13 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: value of interger
- * Return: Absolute value
- * Auth- OLUMOYIN JOSHUA 
+ * @n:value of interger
+ * Return:Absolute value
+ * Auth - OLUMOYIN JOSHUA 
  */
-
 int _abs(int n)
 {
 	if (n > 0)
 		return (n);
-	return (-n);
+return (-n);
 }
