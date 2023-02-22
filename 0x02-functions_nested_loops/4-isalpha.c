61a87a2194e7631493375d09cae2ccc9fdda50a0
olumoyin titobioluwa joshua <olumoyinjoshua19609@gmail.com git config --global user.email olumoyinjoshua19609@gmail.com>
2023-02-21 19:09:09 -0800

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,17 @@
-#include"main.h"
+#include <ctype.h>
+#include "main.h"
 
 /**
- * _isalpha - checks if character is a letter
- *            both lowercase or uppercase
+ * _isalpha - checks if an input
+ * @c: character is lower or not
  *
- * @c: takes input from other functions.
- *
- * Return: 1 is c if true else 0
-*/
+ * Return: 1 if true and 0 if false
+ * Auth - OLUMOYIN JOSHUA
+ */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if (isalpha(c))
 		return (1);
 	return (0);
 }
