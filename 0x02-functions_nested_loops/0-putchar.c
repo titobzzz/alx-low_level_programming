c8b4045132d21f38438ce7d36ef125e2ecf549ad
olumoyin titobioluwa joshua <olumoyinjoshua19609@gmail.com git config --global user.email olumoyinjoshua19609@gmail.com>
2023-02-21 16:49:45 -0800

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Task - Prints "_putchar" followed by new line
+ * Auth - ALABI TIMOTHY
+ */
+
+#include "main.h"
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
