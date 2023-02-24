5770b5c1dde67ae176d3fbe1a7ae11bbf73bf2de
olumoyin titobioluwa joshua <olumoyinjoshua19609@gmail.com git config --global user.email olumoyinjoshua19609@gmail.com>
2023-02-23 19:46:40 -0800

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,21 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
  */
-
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int i = n;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		i = -i;
 	}
-	else
+	if (i / 10)
 	{
-		m = n;
+		print_number(i / 10);
 	}
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar(i % 10 + '0');
 }
